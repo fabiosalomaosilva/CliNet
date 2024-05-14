937b529a04bbcb7b8d4fc3d86c5fee09c7fcef3b
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2024-05-14 16:56:15 -0500

    Add project files.

---
+++ CliNet/Models/TerminalCommand.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace CliNet.Models
+{
+
+    public class ContentResponse
+    {
+        public List<TerminalCommand> content { get; set; } = [];
+    }
+
+    public class TerminalCommand
+    {
+        [JsonProperty("tech")]
+        public string? tech { get; set; }
+
+        [JsonProperty("commands")]
+        public List<string> commands { get; set; } = [];
+    }
+}
