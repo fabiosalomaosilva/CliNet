fb671b90a1096003a4f5dde789a87f3e2bfb1df8
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2024-05-22 14:50:42 -0500

    test: criação do projeto de testes

---
+++ CliNet.Test/GptServiceTests.cs
@@ -0,0 +1,14 @@
+namespace CliNet.Test
+{
+    public class GptServiceTests
+    {
+        [Fact]
+        public async Task CallApiGpt_ShouldReturnListOfTerminalCommands()
+        {
+            var result = true;
+
+            Assert.True(result);
+        }
+
+    }
+}
